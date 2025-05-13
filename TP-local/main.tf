466e0b43387b249df6ea7bd063567bdaf943332b
unknown <farouxbrassart.solenn@gmail.com>
2025-05-13 15:58:46 +0200

    Ajout du TP local 2

--- TP-local/main.tf
+++ TP-local/main.tf
@@ -6,10 +6,16 @@ terraform {
     }
   }
 }
- 
+
 provider "local" {}
- 
+
 resource "local_file" "test_file" {
   content  = "Hello depuis Terraform !"
   filename = "${path.module}/hello.txt"
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'Commande exécutée !'"
+  }
 }
