6e52001881459fb1802db4240dc213f049964185
unknown <farouxbrassart.solenn@gmail.com>
2025-05-13 17:03:41 +0200

    Ajout exo 1

---
+++ TP-local/Exercice_1/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "tcp://localhost:2375"
+}
+
+resource "docker_image" "nginx" {
+  name         = var.docker_image_name
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  name  = var.container_name
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = var.internal_port
+    external = var.external_port
+  }
+}
